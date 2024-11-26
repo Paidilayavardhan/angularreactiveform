a1a4ba3df9901d494c62c333ab9f749f31486b90
jassu7093 <jaswanthpoondru@gmail.com>
2024-11-26 12:49:30 +0530

    commit change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { SignUpComponent } from './signup/signup.component';
+import { SignUpReactiveComponent } from './signup-reactive/signup-reactive.component';
+import { LoginReactiveComponent } from './login-reactive/login-reactive.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,LoginComponent,SignUpComponent,SignUpReactiveComponent,LoginReactiveComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { SignUpComponent } from './signup/signup.component';
+import { LoginComponent } from './login/login.component';
+import { SignUpReactiveComponent } from './signup-reactive/signup-reactive.component';
+import { LoginReactiveComponent } from './login-reactive/login-reactive.component';
+
+export const routes: Routes = [
+  { path: 'signup', component: SignUpComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'signup-reactive', component: SignUpReactiveComponent },
+  { path: 'login-reactive', component: LoginReactiveComponent },
+  { path: '', redirectTo: '/signup', pathMatch: 'full' }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
